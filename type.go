54ccc3041f725c17e5cc72ae3a61de67d953d715
fe0b6 <spirit-dnk@yandex.ru>
2018-04-29 10:12:00 +0300

    init

---
+++ type.go
@@ -0,0 +1,74 @@
+package yamoney
+
+import "encoding/json"
+
+// API - Объект платежа
+type API struct {
+	ShopID int
+	Secret string
+}
+
+// InitObj - объект создания платежа
+type InitObj struct {
+	InvoiceID    string       `json:"-"`
+	Amount       Amount       `json:"amount"`
+	Confirmation Confirmation `json:"confirmation"`
+	Description  string       `json:"description"`
+	PaymentID    string       `json:"payment_id"`
+}
+
+// Amount - объект суммы платежа
+type Amount struct {
+	Value    string `json:"value"`
+	Currency string `json:"currency"`
+}
+
+// Confirmation - объект редиректа
+type Confirmation struct {
+	Type            string `json:"type"`
+	ReturnURL       string `json:"return_url"`
+	ConfirmationURL string `json:"confirmation_url"`
+}
+
+// PaymentInfo - объект ответа при создании платежа
+type PaymentInfo struct {
+	ID                  string          `json:"id"`
+	Status              string          `json:"status"`
+	Description         string          `json:"description"`
+	Paid                bool            `json:"paid"`
+	Amount              Amount          `json:"amount"`
+	RefundedAmount      Amount          `json:"refunded_amount"`
+	Confirmation        Confirmation    `json:"confirmation"`
+	CreatedAt           string          `json:"created_at"`
+	CapturedAt          string          `json:"captured_at"`
+	ExpiresAt           string          `json:"expires_at"`
+	Metadata            json.RawMessage `json:"metadata"`
+	PaymentMethod       PaymentMethod   `json:"payment_method"`
+	Recipient           Recipient       `json:"recipient"`
+	Test                bool            `json:"test"`
+	ReceiptRegistration string          `json:"receipt_registration"`
+}
+
+// PaymentMethod - объект информации о методе оплаты
+type PaymentMethod struct {
+	ID    string `json:"id"`
+	Type  string `json:"type"`
+	Saved bool   `json:"saved"`
+	Card  Card   `json:"card"`
+	Title string `json:"title"`
+}
+
+// Card - информация о карте
+type Card struct {
+	First6      string `json:"first6"`
+	Last4       string `json:"last4"`
+	ExpiryMonth string `json:"expiry_month"`
+	ExpiryYear  string `json:"expiry_year"`
+	CardType    string `json:"card_type"`
+}
+
+// Recipient - Информация о получателе платежа
+type Recipient struct {
+	AccountID string `json:"account_id"`
+	GatewayID string `json:"gateway_id"`
+}
---
+++ yamoney.go
@@ -0,0 +1,162 @@
+package yamoney
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const (
+	paymentURL      = "https://payment.yandex.net/api/v3/payments/"
+	refundURL       = "https://payment.yandex.net/api/v3/refunds/"
+	defaultCurrency = "RUB"
+)
+
+// CreatePayment - Создание платежа
+func (ya *API) CreatePayment(id string, o *InitObj) (ans PaymentInfo, err error) {
+	if o.Amount.Currency == "" {
+		o.Amount.Currency = defaultCurrency
+	}
+
+	if o.Confirmation.Type == "" {
+		o.Confirmation.Type = "redirect"
+	}
+
+	b, err := json.Marshal(o)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Формируем запрос
+	req, err := http.NewRequest("POST", paymentURL, bytes.NewBuffer(b))
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Добавляем заголовое о том что это json
+	req.Header.Set("Content-Type", "application/json")
+	// Добавляем уникальный ключ
+	req.Header.Set("Idempotence-Key", createIdempotenceKey(id, "create"))
+
+	return ya.sendRq(req)
+}
+
+// Capture - Подтверждаем платеж
+func (ya *API) Capture(id string, o *InitObj) (ans PaymentInfo, err error) {
+	if o.Amount.Currency == "" {
+		o.Amount.Currency = defaultCurrency
+	}
+
+	b, err := json.Marshal(o)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Формируем запрос
+	req, err := http.NewRequest("POST", paymentURL+o.InvoiceID+"/capture", bytes.NewBuffer(b))
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Добавляем заголовое о том что это json
+	req.Header.Set("Content-Type", "application/json")
+	// Добавляем уникальный ключ
+	req.Header.Set("Idempotence-Key", createIdempotenceKey(id, "capture"))
+
+	return ya.sendRq(req)
+}
+
+// Refund - Возврат средств
+func (ya *API) Refund(id string, o *InitObj) (ans PaymentInfo, err error) {
+	if o.Amount.Currency == "" {
+		o.Amount.Currency = defaultCurrency
+	}
+
+	b, err := json.Marshal(o)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Формируем запрос
+	req, err := http.NewRequest("POST", refundURL, bytes.NewBuffer(b))
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Добавляем заголовое о том что это json
+	req.Header.Set("Content-Type", "application/json")
+	// Добавляем уникальный ключ
+	req.Header.Set("Idempotence-Key", createIdempotenceKey(id, "refund"))
+
+	return ya.sendRq(req)
+}
+
+// GetPaymentInfo - получаем информацию о платеже
+func (ya *API) GetPaymentInfo(invoiceID string) (ans PaymentInfo, err error) {
+
+	// Формируем запрос
+	req, err := http.NewRequest("GET", paymentURL+invoiceID, nil)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	return ya.sendRq(req)
+}
+
+func (ya *API) sendRq(req *http.Request) (ans PaymentInfo, err error) {
+
+	// Добавляем авторизацию
+	req.SetBasicAuth(strconv.Itoa(ya.ShopID), ya.Secret)
+
+	// Делаем запрос
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Если проблема с ответом
+	if resp.StatusCode != 200 && resp.StatusCode != 202 {
+		err = errors.New(resp.Status)
+		log.Println("[error]", resp.Status, resp.StatusCode)
+	}
+
+	// Читаем ответ
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		log.Println("[error]", err, string(content))
+	}
+
+	// Парсим ответ
+	err = json.Unmarshal(content, &ans)
+	if err != nil {
+		log.Println("[error]", err, string(content))
+		return
+	}
+
+	return
+}
+
+func createIdempotenceKey(id, method string) string {
+	return method + "_" + id
+}
