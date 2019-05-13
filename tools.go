75dab1b30127e086d7300a55889c36ab9eb59744
fe0b6 <spirit-dnk@yandex.ru>
2019-05-13 10:03:51 +0300

    up

--- tools.go
+++ tools.go
@@ -10,14 +10,14 @@ import (
 var noNumReg = regexp.MustCompile("\\D")
 
 // ParsePhone - Привозим телефон к нужному формату
-func ParsePhone(phone string) int {
+func ParsePhone(phone string) int64 {
 	phone = noNumReg.ReplaceAllString(phone, "")
 
 	if strings.HasPrefix(phone, "8") {
 		phone = "7" + strings.TrimPrefix(phone, "8")
 	}
 
-	ans, err := strconv.Atoi(phone)
+	ans, err := strconv.ParseInt(phone, 10, 64)
 	if err != nil {
 		log.Println("[error]", err, phone)
 		return ans
